7a58c2e738ac39eb3b6448841c265e2d7fbc4565
remi.vezy <VEZY@users.noreply.github.com>
2020-04-13 18:55:48 +0200

    Add checks to not watter for too long, and not watter every too short periods

--- plant_watering.ino
+++ plant_watering.ino
@@ -30,6 +30,13 @@ int moisture_perc= 0;
 const int AirValue = 780;   // Driest value -> air value
 const int WaterValue = 280;  // Wettest value -> water value
 
+// Timer to avoid wattering for too long: 
+int watering_max_time = 300000;       // Maximal time for one wattering event (5 minutes)
+int watering_interval_min = 18000000; // Minimal time to wait before two waterring events (5 hours here)
+long last_watering_start= 0.0 ; // time since the waterring event as been started
+long last_watering_end= -watering_interval_min; // time since the last waterring event as been ended
+
+// NB: initialized at -watering_interval_min to be able to start wattering at startup.
 void setup() {
   // Set up the connexion to see the values from the moisture sensor:
   Serial.begin(9600);
@@ -69,20 +76,46 @@ void loop() {
     is_level_change = true;
   }
 
+  if (is_level_change) {
+    // Sending a message via Telegram (only if it wasn't said already, and it didn't change just before)
+    if ((millis() > Bot_lasttime + Bot_mtbs))  {
+      myBot.sendMessage(chatID, "Thank you for refilling the water tank !");
+      Bot_lasttime = millis();
+    }
+  }
+  
   if (water_level == HIGH) {
     // If there is water in the tank...
     // Print the moisture to the console: 
     Serial.print((String)"Soil Moisture : "+moisture_perc+"%"+"("+moisture+")"+"\n");
 
-    if (is_level_change) {
-      // Sending a message via Telegram (only if it wasn't said already, and it didn't change just before)
-      if ((millis() > Bot_lasttime + Bot_mtbs))  {
-        myBot.sendMessage(chatID, "Thank you for refilling the water tank !");
-        Bot_lasttime = millis();
-      }
+    if ((millis() > last_watering_end + watering_interval_min) && (moisture_perc <= 30)) {
+      // Waterring can only start after a minimum given period of time (watering_interval_min)
+      last_watering_start= millis(); // starting wattering
+
+      myBot.sendMessage(chatID, "Starting plant wattering !");
+      
+      while (moisture_perc <= 30) {
+        digitalWrite(relay, HIGH);
+        moisture = analogRead(soil_moisture);
+        moisture_perc = map(moisture,WaterValue,AirValue,100,0);
+        
+        if(millis() > last_watering_start + watering_max_time){
+          // If the system is wattering for more than watering_max_time, stop wattering
+          myBot.sendMessage(chatID, (String)"Plants were wattered during "+((last_watering_end-last_watering_start)/60000)+" minutes but they are still thirsty. Try to set watering_max_time to a higher value");
+          break;
+        }
+        delay(500);                // 500ms delay
+      } 
+      
+      last_watering_end= millis();  
+      myBot.sendMessage(chatID, (String)"Wattered the plants during "+((last_watering_end-last_watering_start)/1000)+" seconds");
+    } else if (moisture_perc <= 30) {
+      myBot.sendMessage(chatID, (String)"Plants are thirsty but were wattered "+((millis()-last_watering_end)/3600000)+" hours ago. Try to set watering_interval_min to a lower value");
     }
-    if (moisture_perc <= 30) {
-      // ...and if the soil is dry, turn on the water pump
+       
+    if ((moisture_perc <= 30) && (millis() > last_watering_start + watering_max_time)) {
+      // ...and if the soil is dry, turn on the water pump (only if the total watering time is less than watering_max_time)
       digitalWrite(relay, HIGH);
     } else {
       // ...and if the soil is wet, do nothing
@@ -101,6 +134,10 @@ void loop() {
     }
   }
 
+
+  // Always shut off the water pump in any other case:
+  digitalWrite(relay, LOW);
+    
   prev_water_level= water_level;
   delay(interval);    
 }
