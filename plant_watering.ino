d584627b97dadfa81709d474a721ccb91efa6cb5
remi.vezy <VEZY@users.noreply.github.com>
2020-04-19 11:53:28 +0200

    Add Inline Keyboard to know if system is ON or to return the soil moisture

--- plant_watering.ino
+++ plant_watering.ino
@@ -9,6 +9,11 @@ int chatID = -000000000; // This the ID of our group chat for the plants
 
 // Instantiate a bot object:
 CTBot myBot;
+CTBotInlineKeyboard myKbd;  // custom inline keyboard object helper
+
+#define LIGHT_ON_CALLBACK  "IsSystemON"  // callback data sent when "LIGHT ON" button is pressed
+#define LIGHT_OFF_CALLBACK "lightOFF" // callback data sent when "LIGHT OFF" button is pressed
+
 int Bot_mtbs = 1000; //mean time between scan messages
 long Bot_lasttime;   //last time messages' scan has been done
 
@@ -48,7 +53,7 @@ void setup() {
   pinMode(tank_floater, INPUT_PULLUP);
   
   Serial.println("Starting TelegramBot...");
-  
+  myBot.setMaxConnectionRetries(15); // try 15 times to connect to the specified SSID (mySSID)
   // connect the ESP8266 to the desired access point
   myBot.wifiConnect(ssid, pass);
 
@@ -61,6 +66,17 @@ void setup() {
   } else {
     Serial.println("\nTelegram connection test: problem");
   }
+
+  
+  // inline keyboard customization for Telegram
+  // add a query button to the first row of the inline keyboard
+  myKbd.addButton("Is The system ON?", LIGHT_ON_CALLBACK, CTBotKeyboardButtonQuery);
+  // add another query button to the first row of the inline keyboard
+  myKbd.addButton("Show soil moisture", LIGHT_OFF_CALLBACK, CTBotKeyboardButtonQuery);
+  // add a new empty button row
+  myKbd.addRow();
+  // add a URL button to the second row of the inline keyboard
+  myKbd.addButton("see docs", "https://github.com/shurillu/CTBot", CTBotKeyboardButtonURL);
 }
 
 void loop() {
@@ -72,7 +88,33 @@ void loop() {
   water_level= digitalRead(tank_floater);
   TBMessage msg; // Store telegram message
 
-  // myBot.getNewMessage(msg);   
+  // if there is an incoming message from Telegram:
+  if (myBot.getNewMessage(msg)) {
+    // check what kind of message was received
+    if (msg.messageType == CTBotMessageText) {
+      // received a text message
+      if (msg.text.equalsIgnoreCase("show keyboard")) {
+        // the user is asking to show the inline keyboard --> show it
+        myBot.sendMessage(msg.sender.id, "Inline Keyboard", myKbd);
+      } else {
+        // the user write anithing else --> show a hint message
+        myBot.sendMessage(msg.sender.id, "Try 'show keyboard' to get the keyboard options.");
+      }
+    } else if (msg.messageType == CTBotMessageQuery) {
+      // received a callback query message
+      if (msg.callbackQueryData.equals(LIGHT_ON_CALLBACK)) {
+        // Say if the system is running: 
+        myBot.endQuery(msg.callbackQueryID, "The system is up and running!", true);
+      } else if (msg.callbackQueryData.equals(LIGHT_OFF_CALLBACK)) {
+        // Return the soil moisture as a popup message
+        myBot.endQuery(msg.callbackQueryID, (String)"Soil Moisture : "+moisture_perc+"%"+"("+moisture+")");
+      }
+    }
+  }
+
+
+
+  
   if (water_level == prev_water_level) {
     is_level_change = false;
   } else {
